4d3f2da1a1555b53cef8f2ba89ee59fbb22cfddc
Matthew <mattalexshaw@gmail.com>
2020-04-01 20:10:37 +0100

    add exercise to replace the value of a variable

--- week_01/exercises.py
+++ week_01/exercises.py
@@ -7,12 +7,15 @@
 # 3. Print out the number you converted above
 
 
-# 4. Ask the user to enter their name, and print "Hello NAME", eg, if I entered
+# 4. Change the number in the variable from exercises 2 and 3 and print it out again
+
+
+# 5. Ask the user to enter their name, and print "Hello NAME", eg, if I entered
 #    "Matthew", it should print out "Hello Matthew"
 
 
-# 5. Create a new variable called "second" which stores 5 times the value of first
+# 6. Create a new variable called "second" which stores 5 times the value of first
 first = 8
 
 
-# 6. Check if the value of "second" is greater than 30. If it is, print out "Yes"
+# 7. Check if the value of "second" is greater than 30. If it is, print out "Yes"
