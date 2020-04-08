23f0ae87f8cfae9ea46fb1aabd99f37a70fd67ab
Matthew <mattalexshaw@gmail.com>
2020-04-08 21:54:25 +0100

    add challenge

---
+++ week_02/challenge.py
@@ -0,0 +1,19 @@
+# Maps items to their cost
+shop = {
+    "bread": 1.50,
+    "cheese": 2.30,
+    "egg": 0.30,
+    "fish": 2.50,
+}
+
+shopping_list = [
+    "bread",
+    "egg",
+    "egg",
+    "egg",
+    "fish",
+    "fish",
+]
+
+# Write some code that calculates the cost of the shopping list and prints it out
+# Hint: There is a method on lists for counting the number of times a value appears
