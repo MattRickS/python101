54e5b200005ca135c3096e4301bd2030990215fb
Matthew <mattalexshaw@gmail.com>
2020-04-08 21:21:16 +0100

    add exercises

---
+++ week_02/exercises.py
@@ -0,0 +1,22 @@
+# 1. Create a list of your three favourite foods in order
+
+
+# 2. Print out your favourite food from the list
+
+
+# 3. Add your fourth favourite food
+
+
+# 4. Create a dictionary mapping three items you'd buy in a shop to a float for
+#    the cost of each item
+
+
+# 5. Increase the cost of one of the items by 1.0
+
+
+# 6. Print out all the unique values in this list
+values = [1, 1, 2, 2, 1, 3]
+
+
+# 7. Extract the word "upon" from the sentence
+sentence = "Once upon a time..."
