28cc0a3ebe8684a8f0ee49db5b48d8cd700e103e
Matthew <mattalexshaw@gmail.com>
2020-04-23 21:56:09 +0100

    add exercises

---
+++ week_04/exercises.py
@@ -0,0 +1,16 @@
+# 1. Write a function called add() which takes two positional parameters and
+#    returns their values added together
+
+
+# 2. Write a function called average() which takes a single positional parameter
+#    of a list, that calculates the average value of the list. The average is
+#    calculated by adding all the values together, and dividing by the length of
+#    the list.
+
+
+# 3. Use the add() function from above to calculate the result of:
+#    (51 + 14) + (23 + 80)
+
+
+# 4. Write a function that raise an error (either raise it explicitly or write
+#    some code that causes an error), then call the function and catch the error
