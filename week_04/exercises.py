3047a9ef329b4421e45021f9e0de15fd224737e9
HeyMattRickS <matt.shaw@heyyou.games>
2021-09-13 11:21:10 +0100

    Update function exercises

--- week_04/exercises.py
+++ week_04/exercises.py
@@ -1,18 +1,21 @@
 # 1. Write a function called add() which takes two positional parameters and
 #    returns their values added together
 
+# 2. Use the add() function from above to calculate the result of:
+#    51 + 14 + 23
 
-# 2. Write a function called average() which takes a single positional parameter
-#    of a list, that calculates the average value of the list. The average is
-#    calculated by adding all the values together, and dividing by the length of
-#    the list.
+# 3. Write a function to sum all the numbers in the following list
+example_list = [8, 2, 3, 0, 7]
 
+# 4. Write a function to find the maximum number in a list
 
-# 3. Use the add() function from above to calculate the result of:
-#    (51 + 14) + (23 + 80)
+# 5. Write a function to check if a number falls in a given range, eg,
+#    is_in_range(10, 1, 20) -> True (10 is between 1 and 20)
 
+# 6. Write a function which takes a list and returns a list of the unique values
+#    For example, unique([1,2,2,3,3,3]) -> [1, 2, 3]
 
-# 4. The code below might cause an error if it runs. Write some code to catch
+# 7. The code below might cause an error if it runs. Write some code to catch
 #    and print out any errors that happen
 VALUES = [10, 20, 30]
 
@@ -20,3 +23,6 @@ def calculate_value(index):
     return VALUES[index] * 20
 
 calculate_value(3)
+
+# More example exercises can be found here:
+#   https://www.w3resource.com/python-exercises/python-functions-exercises.php
